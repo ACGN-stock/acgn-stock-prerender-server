e536e114c3af2e0ef66bd07472bbf302e9f91b72
SoftwareSing <SoftwareSing@gmail.com>
2019-03-10 20:35:06 +0800

    使render後的頁面移除script tag

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const prerenderServer = prerender({
   port: process.env.prerenderServerPort || process.env.port || 3900,
   waitAfterLastRequest: process.env.waitAfterLastRequest || 3000
 });
+prerenderServer.use(prerender.removeScriptTags());
 prerenderServer.start();
