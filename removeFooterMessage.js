39f0a9c3fe093e4632d471727700f6d160e7c5d6
SoftwareSing <SoftwareSing@gmail.com>
2019-03-10 21:47:08 +0800

    以更強制的方法搜尋置底訊息

--- removeFooterMessage.js
+++ removeFooterMessage.js
@@ -4,9 +4,13 @@ module.exports = {
 			return next();
 		}
 
-		const matches = req.prerender.content.toString().match(/<div(?:.*?)footer-message-container(?:.*?)>(?:[\S\s]*?)<\/div>/gi);
+		// 搜尋範圍: https://github.com/mrbigmouth/acgn-stock/blob/90fbbc96c49ad501cf888e77352ff5c1f8f7673b/client/layout/footer.html#L4-L13
+		// 現在的檔案: https://github.com/mrbigmouth/acgn-stock/blob/master/client/layout/footer.html
+		const matches = req.prerender.content.toString()
+			.match(/<div class="snackbar-container">(?:[\S\s]*?)<div class="bg-faded text-primary text-right px-3 py-1 rounded">/gi);
 		for (let i = 0; matches && i < matches.length; i++) {
-			req.prerender.content = req.prerender.content.toString().replace(matches[i], '');
+			req.prerender.content = req.prerender.content.toString()
+				.replace(matches[i], '<div class="bg-faded text-primary text-right px-3 py-1 rounded">');
 		}
 
 		next();
